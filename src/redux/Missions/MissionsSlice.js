95237133acdc6d5d3f3686f3e10bd44ca7a8e820
Kingsley Igbor <chidoziek2@gmail.com>
2023-08-04 05:12:13 +0100

    fetched missions and logic to join and leave

--- src/redux/Missions/MissionsSlice.js
+++ src/redux/Missions/MissionsSlice.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const MissionsSlice = () => (
-  <div>
-    This is the Missions Page;
-  </div>
-);
+const initialState = {
+  data: [],
+  loading: false,
+  error: null,
+};
 
-export default MissionsSlice;
+const BASE_URL = 'https://api.spacexdata.com/v3/missions';
+
+const fetchData = createAsyncThunk('data/fetchData', async () => {
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const MissionsSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {
+    joinMissions: (state, action) => {
+      const missionIds = action.payload;
+      state.data = state.data.map((mission) => (mission.mission_id === missionIds
+        ? { ...mission, activeMember: true }
+        : mission));
+    },
+    leaveMission: (state, action) => {
+      const missionIds = action.payload;
+      state.data = state.data.map((mission) => (mission.mission_id === missionIds
+        ? { ...mission, activeMember: false }
+        : mission));
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export { fetchData };
+export const { joinMissions, leaveMission } = MissionsSlice.actions;
+export default MissionsSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './redux/Rockets/RocketsSlice';
+import MissionsReducer from './redux/Missions/MissionsSlice';
 
 const store = configureStore({
   reducer: {
     rocket: rocketReducer,
+    data: MissionsReducer,
   },
 });
 
