fece0a4a77e4e0a93b7b9e9386a372a6ce6b5ff1
JOdiemo <odiemo@gmail.com>
2023-08-04 14:53:49 +0300

    liner errors fixed

--- src/components/MyProfileContainer.js
+++ src/components/MyProfileContainer.js
@@ -6,6 +6,10 @@ const MyProfileContainer = () => (
       <h3 className="profile-title">My Rockets</h3>
       <RocketProfile />
     </div>
+    <div className="missionprofile">
+      <h3 className="profile-title">My Missions</h3>
+      <RocketProfile />
+    </div>
   </div>
 );
 export default MyProfileContainer;
